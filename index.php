9331e1c78b30028566c9f4d14ca04d5830e9e2a8
Gabriel Penke <gabri.penke@gmail.com>
2021-05-18 21:14:47 -0300

    First commit

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Minha landing page</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <header>
+        <div class="container">
+            <div><div class="logo1"></div></div><!-- logo1 -->
+            <div><div class="logo2"></div></div><!-- logo2 -->
+            <div><div class="clear"></div></div><!-- clear -->
+        </div><!-- container -->    
+    </header>
+
+    <section class="cover-form">
+        <div class="container bg">
+            <div class="form-container">
+                <h2>Ainda mais belo por dentro</h2>
+                <form action="">
+                    <div class="form-wraper">
+                        <label for="name">Nome Completo</label>
+                        <input type="text" name='name' required placeholder='Digite seu nome'>
+                    </div><!-- form wrapper -->
+
+                    <div class="form-wraper">
+                        <label for="name">E-mail</label>
+                        <input type="text" name='email' required placeholder='Digite seu E-mail'>
+                    </div><!-- form wrapper -->
+
+                    <div class="form-wraper">
+                        <label for="name">Telefone</label>
+                        <input type="text" name='phone' required placeholder='Digite seu Telefone'>
+                    </div><!-- form wrapper -->
+
+                    <div class="form-wraper">
+                        <input type='submit' value='Enviar' name='action'>
+                    </div><!-- form wrapper -->
+
+                    <div class="form-wraper">
+                        <p>* Campos Obrigatórios</p>
+                    </div><!-- form wrapper -->
+                </form>
+            </div>
+        </div>
+    </section><!-- cover form -->
+
+</body>
+</html>
